9496b64c89fd6386787bc149aecfe86a63d196ac
luoxi <luoxi@weihong.com.cn>
2019-09-23 15:08:44 +0800

    add vue test

---
+++ vuetest/src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="hello">
+    <h1>site: {{site}}</h1>
+    <h1>url: {{url}}</h1>
+    <h1>{{details()}}</h1>
+  </div>
+</template>
+
+<script type="text/javascript">
+const vm = new Vue({
+  el: '#hello',
+  data: {
+    site: '你好',
+    url: 'www',
+    alexa: '10000',
+  },
+  methods: {
+    details: function() {
+      return this.site + '-虚无之境';
+    }
+  }
+})
+</script>
