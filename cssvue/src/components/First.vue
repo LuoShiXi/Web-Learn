207caf214bcba49b1de098e5278cd14251a41933
LuoShiXi <rocyluoxi@163.com>
2019-09-27 15:57:56 +0800

    add 拖放

--- cssvue/src/components/First.vue
+++ cssvue/src/components/First.vue
@@ -1,7 +1,15 @@
 <template>
   <body>
-    <div class="first">
+    <div class="first" @dragover="onDragOver($event)" @drop="onDrop($event)">
       <h1>第一个界面</h1>
+    </div>
+    <br>
+    <br>
+    <div class="parent">
+      <span id="dragspan" draggable="true" @dragstart="onDragStart($event)">
+        draggable-span
+      </span>
+      <span id="dropzone"> dropzone </span>
     </div>
   </body>
 </template>
@@ -12,6 +20,29 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    onDragStart (event) {
+      event
+        .dataTransfer
+        .setData('text/plain', event.target.id);
+
+      event
+        .currentTarget
+        .style
+        .backgroundColor = 'yellow';
+    },
+    onDragOver (event) {
+      event.preventDefault();
+    },
+    onDrop (event) {
+      const id = event.dataTransfer.getData('text');
+      console.log(id);
+      const draggableElement = document.getElementById(id);
+      const dropzone = event.target;
+      dropzone.appendChild(draggableElement);
+      event.dataTransfer.clearData();
     }
   }
 }
